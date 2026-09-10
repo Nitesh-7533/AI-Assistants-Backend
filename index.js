21d68a0ee2981cfd451e0cca28a5ff4130805b48
Nitesh kumar <nkumar49575@gmail.com>
2026-09-10 10:29:15 +0530

    Update CORS configuration to use an array of origins

--- index.js
+++ index.js
@@ -4,24 +4,26 @@ configDotenv();
 import connectDb from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
 import cookieParser from "cookie-parser";
-
 import cors from "cors";
 import userRouter from "./routes/user.routes.js";
 import geminiResponse from "./gemini.js";
 
 const app = express();
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
-    "https://ai-assistant-frontend-1.onrender.com",
+    origin: [
+      "http://localhost:5173",
+      "https://ai-assistant-frontend-1.onrender.com"
+    ],
     credentials: true,
-  }),
+  })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
 const port = process.env.PORT || 5000;
-
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
